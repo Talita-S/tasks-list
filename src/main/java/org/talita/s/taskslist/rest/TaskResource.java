6263dbbc3d85715f9a26d680a12a6d1bd3f2cbce
Talita-S <talitassantos2015@gmail.com>
2023-12-27 00:56:56 -0300

    refactor: adjusting project architecture

--- src/main/java/org/talita/s/taskslist/rest/TaskResource.java
+++ src/main/java/org/talita/s/taskslist/rest/TaskResource.java
@@ -1,15 +1,22 @@
 package org.talita.s.taskslist.rest;
 
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.talita.s.taskslist.rest.dto.AddTaskRequest;
 
 @Path("/tasks")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class TaskResource {
 
     @POST
     public Response addTask (AddTaskRequest taskRequest ) {
+        return Response.ok(taskRequest).build();
+    }
+
+    @GET
+    public Response listAllTasks() {
         return Response.ok().build();
     }
 
