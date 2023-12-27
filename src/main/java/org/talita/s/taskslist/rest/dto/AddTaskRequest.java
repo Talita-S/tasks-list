ff04a4aba528a1f8c6769de2d4a8379e142ee688
Talita-S <talitassantos2015@gmail.com>
2023-12-26 21:05:24 -0300

    feat: TaskRequest created

---
+++ src/main/java/org/talita/s/taskslist/rest/dto/AddTaskRequest.java
@@ -0,0 +1,23 @@
+package org.talita.s.taskslist.rest.dto;
+
+public class AddTaskRequest {
+
+    private String description;
+    private boolean done;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
