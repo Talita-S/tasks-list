40f876a3ea7707f03db1f6179a90abf1745c1f52
Talita-S <talitassantos2015@gmail.com>
2023-12-31 01:21:38 -0300

    test: adding addTaskValidationErrorTest

--- src/test/java/org/talita/s/taskslist/rest/TaskResourceTest.java
+++ src/test/java/org/talita/s/taskslist/rest/TaskResourceTest.java
@@ -5,6 +5,10 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.talita.s.taskslist.rest.dto.AddTaskRequest;
+import org.talita.s.taskslist.rest.dto.ResponseError;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,6 +34,28 @@ class TaskResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("Should return error when json isn't valid")
+    void addTaskValidationErrorTest() {
+        var task = new AddTaskRequest();
+        task.setDescription(null);
+        task.setDone(false);
+
+        var response =
+                given()
+                    .contentType(ContentType.JSON)
+                    .body(task)
+                .when()
+                    .post("/tasks")
+                .then()
+                    .extract().response();
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Erro de validação", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
     }
 
 }
